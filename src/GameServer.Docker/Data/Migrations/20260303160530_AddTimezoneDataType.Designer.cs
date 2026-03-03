89b41cd56fe8d9922c6c47ce2f11baa46c949672
Michael Bulava <mbulava@gmail.com>
2026-03-03 10:06:27 -0600

    fix: Add timezone data type to database constraint
    Fixes SQLite exception when saving extended metadata with timezone data type. Added timezone to CK_SettingsMetadata_DataType check constraint. Created DbContext factory for EF Core design-time operations. Added migration to update existing databases.

--- src/GameServer.Docker/Data/Migrations/20260303160530_AddTimezoneDataType.Designer.cs
+++ src/GameServer.Docker/Data/Migrations/20260303160530_AddTimezoneDataType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GameServer.Docker.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GameServer.Docker.Data.Migrations
 {
     [DbContext(typeof(GameServerDbContext))]
-    partial class GameServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260303160530_AddTimezoneDataType")]
+    partial class AddTimezoneDataType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.3");
